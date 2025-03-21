54aeafece79e327e162ab9eec57f9982268a4351
Mac <61571756+colby1394@users.noreply.github.com>
2025-03-20 23:29:10 -0700

    Update gcpscript.sh

--- gcpscript.sh
+++ gcpscript.sh
@@ -6,16 +6,18 @@ yum install -y httpd nano
 systemctl start httpd
 systemctl enable httpd
 
-
+# Docs for GCP metadata: https://cloud.google.com/compute/docs/metadata/predefined-metadata-keys
 
 # Background the curl requests
-curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google" &> /tmp/local_ipv4 &
+curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google" &> /tmp/public_ipv4 &
 curl -s "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google" &> /tmp/az &
 curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/mac" -H "Metadata-Flavor: Google" &> /tmp/macid &
+curl -s "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip" -H "Metadata-Flavor: Google" &> /tmp/local_ipv4 &
 wait
 
 macid=$(cat /tmp/macid)
 local_ipv4=$(cat /tmp/local_ipv4)
+public_ipv4=$(cat /tmp/public_ipv4)
 az=$(cat /tmp/az)
 vpc=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/id" -H "Metadata-Flavor: Google")
 
@@ -31,25 +33,33 @@ echo "
 <h1>Starfleet Cluster</h1>
 
 <br>
-<img src="https://raw.githubusercontent.com/colby1394/GCP_Mini_Website/refs/heads/main/Starfleet_Command_insignia%252C_2400s.webp?token=GHSAT0AAAAAADAFZLQP4L2CR6VGN2PZXAW2Z644RJA" alt="StarfleetLogo" width="200" height="200">
+<img src="https://raw.githubusercontent.com/colby1394/GCP_Mini_Website_Public/refs/heads/main/Starfleet_Command_insignia.webp" alt="StarfleetLogo" width="200" height="200">
 <br>
 
-<p><b>Instance Name:</b> $(hostname -f) </p>
+<p><b>Instance Hostname:</b> $(hostname -f) </p>
 <p><b>Instance Private Ip Address: </b> ${local_ipv4}</p>
-<p><b>Availability Zone: </b> ${az}</p>
+<p><b>Instance Public Ip Address: </b> ${public_ipv4}</p>
+<p><b>Instance MAC Address: </b> ${macid}</p>
+<p><b>Zone: </b> ${az}</p>
 <p><b>Virtual Private Cloud (VPC):</b> ${vpc}</p>
+</div>
+
+<div>
+<h1> Your IP Address</h1>
+<a href="http://www.ipchicken.com">
+<img src="http://www.ipchicken.com/images/ipc.gif" width="88" height="31" border="0"></a>
 </div>
 </body>
 </html>
 " > /var/www/html/index.html
 
 # Clean up the temp files
-rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
+rm -f /tmp/local_ipv4 /tmp/az /tmp/macid /tmp/public_ipv4
 
 
 # Get ex ip #curl "http://metadata.google.internal/computeMetadata/v1/instk-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google"
 #curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "Metadata-Flavor: Google"
-# Get local ip #curl "http://metadata.google.internal/computeMetadata/v1/instk-interfaces/0/ip" -H "Metadata-Flavor: Google"
+# Get local ip #curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip" -H "Metadata-Flavor: Google"
 # curl "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google"
 # Get instance name #curl "http://metadata.google.internal/computeMetadata/v1/instk-name" -H "Metadata-Flavor: Google"
 # Get instance zone #curl "http://metadata.google.internal/computeMetadata/v1/instk-zone" -H "Metadata-Flavor: Google"
